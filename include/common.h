adb6f82276b16b8c3a409f89eeea57caae0d0a5d
Carlos Henrique Macedo dos Santos - PrestServ <carlos.santos54.prestserv+petro@petrobras.com.br>
2025-08-07 03:12:11 -0300

    fixing waves

--- include/common.h
+++ include/common.h
@@ -53,7 +53,7 @@
 //
 //--------------------------------------------------------------
 
-#define MAX_WAVES 1 // NUMERO DE WAVES
+#define MAX_WAVES 50 // NUMERO DE WAVES
 #define ENEMY_LINE_SPAWN_START	-80
 #define MAX_WEAPON_SLOTS		3
 
